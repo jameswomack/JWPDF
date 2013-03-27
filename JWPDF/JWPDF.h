1a1c051caede99c617b00f138e167a1816d2a36a
James Womack <jamesw@amcoonline.net>
2013-03-27 13:33:16 -0700

    Ignoramus

---
+++ JWPDF/JWPDF.h
@@ -0,0 +1,13 @@
+//
+//  JWPDF.h
+//  JWPDF
+//
+//  Created by James Womack on 3/27/13.
+//  Copyright (c) 2013 Noble Gesture. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JWPDF : NSObject
+
+@end
---
+++ JWPDF/JWPDF.m
@@ -0,0 +1,13 @@
+//
+//  JWPDF.m
+//  JWPDF
+//
+//  Created by James Womack on 3/27/13.
+//  Copyright (c) 2013 Noble Gesture. All rights reserved.
+//
+
+#import "JWPDF.h"
+
+@implementation JWPDF
+
+@end
