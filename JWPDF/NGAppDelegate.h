baaa01c89591decd246541c60b1768cd17ecb8e2
James Womack <jamesw@amcoonline.net>
2013-03-27 13:16:49 -0700

    Initial Commit

---
+++ JWPDF/NGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NGAppDelegate.h
+//  JWPDF
+//
+//  Created by James Womack on 3/27/13.
+//  Copyright (c) 2013 Noble Gesture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JWPDF/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JWPDF
+//
+//  Created by James Womack on 3/27/13.
+//  Copyright (c) 2013 Noble Gesture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NGAppDelegate class]));
+    }
+}
